9f4463ffd5284aff2c85c6d6561cba861ffe7220
HagegeR <Hagege.ruben@gmail.com>
2020-02-23 00:16:43 +0200

    added eigen on make flags

--- build_opencv.sh
+++ build_opencv.sh
@@ -110,6 +110,7 @@ configure () {
         -D CUDA_ARCH_BIN=5.3,6.2,7.2
         -D CUDA_ARCH_PTX=
         -D CUDA_FAST_MATH=ON
+        -D EIGEN_INCLUDE_PATH=/usr/include/eigen3
         -D ENABLE_FAST_MATH=ON 
         -D ENABLE_NEON=ON
         -D OPENCV_ENABLE_NONFREE=ON
