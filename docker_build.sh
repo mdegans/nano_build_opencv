70283c3fe88694aec448d17c3c740ebba80bc7c1
Michael de Gans <47511965+mdegans@users.noreply.github.com>
2020-07-13 11:50:02 -0700

    JetPack 4.4 GA update (#38)
    * changes necessary to build under JetPack 4.4 with the new base image.
    * tag syntax update (multiple -t rather than a separate docker tag). This may not work on older docker versions, but this is not applicable to recent JetPacks.

--- docker_build.sh
+++ docker_build.sh
@@ -3,15 +3,14 @@
 set -ex
 
 # just change these to bump the version
-readonly JETPACK_VERSION="r32.4.2"
-readonly OPENCV_VERSION="4.3.0"
+readonly JETPACK_VERSION="r32.4.3"
+readonly OPENCV_VERSION="master"
 
 # build the image
-docker build \
+docker build --pull \
     --build-arg OPENCV_BUILD_JOBS=$(nproc) \
     --build-arg JETPACK_VERSION=${JETPACK_VERSION} \
     --build-arg OPENCV_VERSION=${OPENCV_VERSION} \
     -t mdegans/tegra-opencv:jp-${JETPACK_VERSION}-cv-${OPENCV_VERSION} \
+    -t mdegans/tegra-opencv:latest \
     . 2>&1 | tee build.log
-# tag it as latest
-docker tag mdegans/tegra-opencv:jp-${JETPACK_VERSION}-cv-${OPENCV_VERSION} mdegans/tegra-opencv:latest
